826c75641fa075d451864055a4084590f69c07c7
Merlin Bögershausen <merlin.boegershausen@rwth-aachen.de>
2025-01-30 11:23:59 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+![Logo](https://github.com/openrewrite/rewrite/raw/main/doc/logo-oss.png)
+### Migrate OpenRewrite Recipe projects. Automatically.
+
+[![ci](https://github.com/openrewrite/rewrite-rewrite/actions/workflows/ci.yml/badge.svg)](https://github.com/openrewrite/rewrite-rewrite/actions/workflows/ci.yml)
+[![Maven Central](https://img.shields.io/maven-central/v/org.openrewrite.recipe/rewrite-rewrite.svg)](https://mvnrepository.com/artifact/org.openrewrite.recipe/rewrite-rewrite)
+
+### What is this?
+
+This project implements an [Rewrite module](https://github.com/openrewrite/rewrite) that performs common tasks for OpenRewrite recipes.
+
+Browse [a selection of recipes available through this module in the recipe catalog](https://docs.openrewrite.org/recipes/java/Recipe).
+
+## Contributing
+
+We appreciate all types of contributions. See the [contributing guide](https://github.com/openrewrite/.github/blob/main/CONTRIBUTING.md) for detailed instructions on how to get started.
---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("org.openrewrite.build.recipe-library") version "latest.release"
+    id("org.openrewrite.build.moderne-source-available-license") version "latest.release"
+}
+
+group = "org.openrewrite.recipe"
+description = "A rewrite module automating best practices and major version migrations for OpenRewrite recipes"
+
+val rewriteVersion = rewriteRecipe.rewriteVersion.get()
+dependencies {
+    implementation(platform("org.openrewrite:rewrite-bom:$rewriteVersion"))
+
+    implementation("org.openrewrite:rewrite-java")
+    implementation("org.openrewrite.recipe:rewrite-migrate-java:$rewriteVersion")
+    implementation("org.openrewrite.recipe:rewrite-static-analysis:$rewriteVersion")
+    implementation("org.openrewrite.recipe:rewrite-testing-frameworks:$rewriteVersion")
+
+    runtimeOnly("org.openrewrite.recipe:rewrite-java-dependencies:$rewriteVersion")
+
+    testImplementation("org.openrewrite:rewrite-test")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:latest.release")
+    testImplementation("org.junit-pioneer:junit-pioneer:2.3.0")
+}
+
+nexusPublishing {
+    repositories.getByName("sonatype") {
+        nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+        snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+    }
+}
