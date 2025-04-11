ccec5be1080261e7c0c75fc553705912ff594d37
Jammy Louie <jammy@moderne.io>
2025-04-11 14:09:54 -0400

    Initial commit with tests

---
+++ src/main/java/org/openrewrite/java/recipes/SourceSpecTextBlockNewLine.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2025 the original author or authors.
+ * <p>
+ * Licensed under the Moderne Source Available License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://docs.moderne.io/licensing/moderne-source-available-license
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.recipes;
+
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.Recipe;
+import org.openrewrite.TreeVisitor;
+import org.openrewrite.internal.ListUtils;
+import org.openrewrite.java.JavaIsoVisitor;
+import org.openrewrite.java.tree.J;
+import org.openrewrite.java.tree.Space;
+import org.openrewrite.java.tree.TypeUtils;
+
+import java.util.Arrays;
+import java.util.StringJoiner;
+import java.util.regex.Pattern;
+
+public class SourceSpecTextBlockNewLine extends Recipe {
+    @Override
+    public String getDisplayName() {
+        return "New line at the end of `SourceSpecs` text blocks";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Text blocks that assert before and after source code should have a new line after it is closed.";
+    }
+}
