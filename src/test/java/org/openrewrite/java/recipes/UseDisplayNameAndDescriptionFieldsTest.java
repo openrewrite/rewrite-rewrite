5fe1236928c11c95c9d07e3f7cffca7e10a5d056
Tim te Beek <tim@moderne.io>
2026-01-13 10:40:45 +0100

    Update src/test/java/org/openrewrite/java/recipes/UseDisplayNameAndDescriptionFieldsTest.java
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/test/java/org/openrewrite/java/recipes/UseDisplayNameAndDescriptionFieldsTest.java
+++ src/test/java/org/openrewrite/java/recipes/UseDisplayNameAndDescriptionFieldsTest.java
@@ -18,8 +18,6 @@ package org.openrewrite.java.recipes;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.openrewrite.DocumentExample;
-import org.openrewrite.java.JavaParser;
-import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 
 import static org.openrewrite.java.Assertions.java;
